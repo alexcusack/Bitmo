2a5be6e6c6274f8022c0e01788823a0353f9761e
cusackalex <cusackalex@gmail.com>
2015-06-18 13:59:40 -0700

    listeners for pay and charge button setup

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,39 @@
 $(document).ready(function() {
 
+  // bind pay button
+  $('#pay').on('click', function(event){
+   event.preventDefault;
+   alert('pay caught')
+  });
+
+  $('#charge').on('click', function(event){
+   event.preventDefault;
+   alert('charge caught')
+
+  });
+
 });
 
 
-console.log("your document is ready and linked!");
-var thing = function(){console.log("this is a thing")};
+
+// make a payment
+var makePayment = function(){
+  var action = $('.create-transaction').attr('action');
+  var method = $('.create-transaction').attr('method');
+  var toPerson = $('.to-field').val()
+  var amount = $('.amount-field').val()
+  var description = $('.description-field').val()
+};
+
+
+
+
+
+// make a charge
+var makeCharge = function(){
+  var action = $('.create-transaction').attr('action');
+  var method = $('.create-transaction').attr('method');
+  var toPerson = $('.to-field').val()
+  var amount = $('.amount-field').val()
+  var description = $('.description-field').val()
+};
