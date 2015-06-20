9658acf71e9be3396a3ed97b447dfea12cd8452f
cusackalex <cusackalex@gmail.com>
2015-06-20 14:04:52 -0700

    add account setup links with listeners

---
+++ public/js/accountSetup.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $('#coinbase').on('click', function(event){event.preventDefault;
+  alert('coinbase linked')});
+
+  $('#venmo').on('click', function(event){event.preventDefault;
+  alert('venmo linked')});
+
+
+
+});
+
