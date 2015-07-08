a43eb0caeacb4b16a41d33c7e29c7931b3424d0f
cusackalex <cusackalex@gmail.com>
2015-07-08 13:44:56 -0700

    remove unused gem

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'oauth2'
 gem 'httparty'
 gem 'coinbase', '4.0.0'
 gem 'omniauth-coinbase', github: 'cusackalex/omniauth-coinbase'
-gem 'omniauth'
+# gem 'omniauth', '1.2.2'
 gem 'omniauth-oauth2'
 
 gem 'shotgun'
--- app/helpers/transaction_helpers.rb
+++ app/helpers/transaction_helpers.rb
@@ -17,9 +17,10 @@ helpers do
     # ignore these errors because it works...
   end
 
-  def venmo_transfer_to_initializing_user
-    uri = URI("https://api.venmo.com/v1/payments?access_token=#{session['venmo_token']['access_token']}&user_id=#{receiver.email}&note=#{params[:description].delete(' ')}&amount=#{params[:amount].to_f/100}")
-    return Transaction.make_venmo_payment(uri)
+  def venmo_transfer_to_initializing_use
+    # need a way to cache my own auth token and pass that access toek in
+    # uri = URI("https://api.venmo.com/v1/payments?access_token=#{session['venmo_token']['access_token']}&user_id=#{current_user.email}&note=#{params[:description].delete(' ')}&amount=#{params[:amount].to_f/100}")
+    # return Transaction.make_venmo_payment(uri)
   end
 
   def venmo_payment_from_currentuser_to_receipant(receiver)
