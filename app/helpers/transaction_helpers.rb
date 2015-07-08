a65200600905e52ca313f2a4a6b05c73dae2edcd
Alex Cusack <cusackalex@gmail.com>
2015-07-08 12:18:01 -0700

    coinbase error rescue added

--- app/helpers/transaction_helpers.rb
+++ app/helpers/transaction_helpers.rb
@@ -1,22 +1,30 @@
 helpers do
 
- def make_coinbase_payemt(params)
-  account = coinbase_client.primary_account
-  if account
-    account.send(to: params[:to], amount: params[:amount], currency: params[:currency], two_factor_token: params[:auth], description: params[:description])
-  else
-    halt! 'current user coinbase account not found'
+  def make_coinbase_payemt(params)
+    account = coinbase_client.primary_account
+    if account
+      account.send(
+        to: params[:to],
+        amount: params[:amount],
+        currency: params[:currency],
+        two_factor_token: params[:auth],
+        description: params[:description],
+      )
+    else
+      halt! 'current user coinbase account not found'
+    end
+  rescue Coinbase::Wallet::InternalServerError
+    # ignore these errors because it works
   end
- end
 
- def venmo_transfer_to_initializing_user
-  uri = URI("https://api.venmo.com/v1/payments?access_token=#{session['venmo_token']['access_token']}&user_id=#{receiver.email}&note=#{params[:description].delete(' ')}&amount=#{params[:amount].to_f/100}")
-  return Transaction.make_venmo_payment(uri)
- end
+  def venmo_transfer_to_initializing_user
+    uri = URI("https://api.venmo.com/v1/payments?access_token=#{session['venmo_token']['access_token']}&user_id=#{receiver.email}&note=#{params[:description].delete(' ')}&amount=#{params[:amount].to_f/100}")
+    return Transaction.make_venmo_payment(uri)
+  end
 
- def venmo_payment_from_currentuser_to_receipant(receiver)
-  uri = URI("https://api.venmo.com/v1/payments?access_token=#{session['venmo_token']['access_token']}&user_id=#{receiver.email}&note=#{params[:description].delete(' ')}&amount=#{params[:amount].to_f/100}")
-  return Transaction.make_venmo_payment(uri)
- end
+  def venmo_payment_from_currentuser_to_receipant(receiver)
+    uri = URI("https://api.venmo.com/v1/payments?access_token=#{session['venmo_token']['access_token']}&user_id=#{receiver.email}&note=#{params[:description].delete(' ')}&amount=#{params[:amount].to_f/100}")
+    return Transaction.make_venmo_payment(uri)
+  end
 
 end
