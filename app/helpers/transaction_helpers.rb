8a55337918f54562a126c1beb57ea702529e76f6
Alex Cusack <cusackalex@gmail.com>
2015-07-14 22:35:26 -0700

    fix user selection in step 1 venmo payment

--- app/helpers/transaction_helpers.rb
+++ app/helpers/transaction_helpers.rb
@@ -20,12 +20,8 @@ helpers do
   end
 
   def venmo_transfer_to_initializing_user
-    p current_user
-    p session
     uri = URI("https://api.venmo.com/v1/payments?access_token=#{AppVenmoAccount.venmo_token}&email=#{current_user.email}&note=#{params[:description].delete(' ')}&amount=#{params[:amount]}")
-    p current_user
-    venmo_response = Transaction.make_venmo_payment(uri)
-    p venmo_response
+    return Transaction.make_venmo_payment(uri)
   end
 
   def venmo_payment_from_currentuser_to_receipant(receiver)
