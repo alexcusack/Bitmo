d7d2c3e43e9ecc99dd2ec62fb16fb267db29c8f9
Alex Cusack <cusackalex@gmail.com>
2015-07-15 12:13:36 -0700

    add alpha tester how-to notes

--- app/controllers/transaction.rb
+++ app/controllers/transaction.rb
@@ -4,9 +4,9 @@ end
 
 post '/transactions' do
   receiver = User.find_or_create_by!(email: params[:to])
-  make_coinbase_payment(params, receiver)
-  venmo_transfer_to_initializing_user
-  venmo_transaction_log = venmo_payment_from_currentuser_to_recipient(receiver)
+  # make_coinbase_payment(params, receiver)
+  # venmo_transfer_to_initializing_user
+  # venmo_transaction_log = venmo_payment_from_currentuser_to_recipient(receiver)
 
   transaction = Transaction.new(
     amount: params[:amount],
@@ -15,7 +15,7 @@ post '/transactions' do
     receiver_id: receiver.id,
     status: "complete",
     transaction_type: receiver.email,
-    venmo_json_response: venmo_transaction_log,
+    # venmo_json_response: venmo_transaction_log,
   )
 
   if transaction.save
