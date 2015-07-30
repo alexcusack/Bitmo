863bb0b675068ddbd60c9209e89627a79bea25b8
Alex Cusack <cusackalex@gmail.com>
2015-07-29 18:42:11 -0700

    turn off live transaction

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-
-  $('.payment-form input[type=submit]').on('click', function(event){
-    var submitButton = $(this);
-    submitButton.closest('form').find('input[name=transaction_type]').val(submitButton.val());
-  });
-
   $('.payment-form').on('submit', function(event){
     event.preventDefault();
     var form = $(this).closest('form');
@@ -15,11 +9,10 @@ $(document).ready(function() {
 
 
   var makePayment = function(form){
-
     var request = $.ajax({
       url: form.attr('action'),
       type: form.attr('method'),
-      data: form.serialize(),
+      data: $("form.payment-form input[type=text]"),
       dataType: 'JSON',
     });
 
