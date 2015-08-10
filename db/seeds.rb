74f878d741a3c3cba8f887f15d0ff830a68f9310
Alex Cusack <cusackalex@gmail.com>
2015-08-10 09:40:53 -0700

    migrate design update

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,8 @@ require 'faker'
 
 20.times do
   User.create(
-     first_name: Faker::Name.first_name,
-     last_name: Faker::Name.last_name,
      username: Faker::Internet.user_name,
      password_hash: 'banana',
-     phone_number: "123-435-0202",
      email: Faker::Internet.email,
      coinbase_account: "12345678",
      venmo_account: "123456"
